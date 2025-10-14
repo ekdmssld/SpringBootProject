83de1407bb50c44cff9d4e6aff40db6167e1c60d
ekdmssld <ekdms8652@gmail.com>
2025-10-14 16:21:00 +0900

    롬복 전

---
+++ 25-2/전자정부/firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,25 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String title;
+
+    public Article(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    @Column
+    private String content;
+}
