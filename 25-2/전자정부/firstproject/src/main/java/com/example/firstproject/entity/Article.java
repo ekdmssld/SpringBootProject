7fab0447ae37fc3e46417e458db39cc58ebc5f79
ekdmssld <ekdms8652@gmail.com>
2025-10-15 16:40:27 +0900

    10장까지 정리 완료

--- 25-2/전자정부/firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ 25-2/전자정부/firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -1,25 +1,26 @@
 package com.example.firstproject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 @Entity
+@Getter
 public class Article {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
     private String title;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
     @Column
     private String content;
+
+    protected Article(){
+
+    }
 }
